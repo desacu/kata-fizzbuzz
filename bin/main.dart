cf5bd684c0436ee0f54da46c7426571ea7fd637f
desacu <noemidsc@gmail.com>
2021-06-18 18:45:10 +0100

    GREEN - return FizzBuzz if number is 15

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,6 @@
 Iterable<dynamic> fizzbuzz() {
   return generateNumberRange().map((number) {
+    if (number == 15) return 'FizzBuzz';
     if (number == 3) return 'Fizz';
     if (number == 5) return 'Buzz';
     return number;
