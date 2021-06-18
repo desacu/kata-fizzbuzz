9010c70043e99413f194882c21135fe28f518fe3
desacu <noemidsc@gmail.com>
2021-06-18 19:43:39 +0100

    REFACTOR - returns the correct combination for the first 20 elements

--- bin/main.dart
+++ bin/main.dart
@@ -1,10 +1,14 @@
 Iterable<dynamic> fizzbuzz() {
-  return generateNumberRange().map((number) {
-    if (number % 15 == 0) return 'FizzBuzz';
-    if (number % 3 == 0) return 'Fizz';
-    if (number % 5 == 0) return 'Buzz';
+  return 1.to(100).map((number) {
+    if (number.isDivisibleBy(15)) return 'FizzBuzz';
+    if (number.isDivisibleBy(3)) return 'Fizz';
+    if (number.isDivisibleBy(5)) return 'Buzz';
     return number;
   });
 }
 
-Iterable<int> generateNumberRange() => [for (var number = 1; number <= 100; number++) number];
+extension DivisibleNumber on int {
+  List<int> to(int maxRange) => [for (var number = 1; number <= maxRange; number++) number];
+
+  bool isDivisibleBy(int toDivide) => (this % toDivide) == 0;
+}
--- test/main_test.dart
+++ test/main_test.dart
@@ -5,7 +5,7 @@ import '../bin/main.dart';
 void main() {
   group('FizzBuzz should', () {
     test('return consecutive numbers from 1 to max value', () {
-      expect(generateNumberRange().take(10), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+      expect(1.to(10), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
     });
 
     test('return Fizz if number is 3', () {
