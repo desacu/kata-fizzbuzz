c4233aca3120a481b1344a8300f3a1707f40adb9
desacu <noemidsc@gmail.com>
2021-06-18 19:11:45 +0100

    GREEN - returns the correct combination for the first 20 elements

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,8 @@
 Iterable<dynamic> fizzbuzz() {
   return generateNumberRange().map((number) {
-    if (number == 15) return 'FizzBuzz';
-    if (number == 3) return 'Fizz';
-    if (number == 5) return 'Buzz';
+    if (number % 15 == 0) return 'FizzBuzz';
+    if (number % 3 == 0) return 'Fizz';
+    if (number % 5 == 0) return 'Buzz';
     return number;
   });
 }
